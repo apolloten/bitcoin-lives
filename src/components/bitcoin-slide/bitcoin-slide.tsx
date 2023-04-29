9318c618a85ce643ef5c9ea8f75938409ee7f3e4
Jay Winder <jay@winder.io>
2023-04-29 16:43:08 +0100

    Updated fonts, added all slides, fixed slide progression.

--- src/components/bitcoin-slide/bitcoin-slide.tsx
+++ src/components/bitcoin-slide/bitcoin-slide.tsx
@@ -8,10 +8,10 @@ import { OrbitControls } from "@react-three/drei"
 import Typewriter from 'typewriter-effect';
 
 const textArray = [
-    "Bitcoin was launched on January 3, 2009. Bitcoin emerged as a response to the financial crisis during 2007-2008, with its decentralised nature and reliance on a peer-to-peer network. It aimed to create a new form of digital money that was not controlled by any single entity and provide more financial stability and freedom.", 
-    "Text 2", 
-    "Text 3", 
-    "Text 4"
+    `Bitcoin was launched on January 3, 2009. Bitcoin emerged as a response to the financial crisis during 2007-2008, with its decentralised nature and reliance on a peer-to-peer network. It aimed to create a new form of digital money that was not controlled by any single entity and provide more financial stability and freedom.`, 
+    `Bitcoin's initial block is referred to as the genesis block." It includes a headline from a British newspaper: "The Times 03/Jan/2009 Chancellor on brink of second bailout for banks." The tax payer's money being repeatedly used to bail out banks that gambled and lost, is the underlying ideological motivation behind the creation of bitcoin.`, 
+    `Limited supply: There can only ever be 21 million Bitcoins in existence. This is in stark contrast to fiat currency which can be printed infinitely by central banks and governments.`, 
+    `Mysterious creator: The identity of Bitcoin's creator, Satoshi Nakamoto, remains unknown, despite various investigations and claims. This is probably an intentional decision knowing that central banks and governments would likely attempt to seize control of the network through the founder(s).`
 ];
 
 const BitcoinSlide = () => {
@@ -29,34 +29,29 @@ const BitcoinSlide = () => {
 
     return (
         <div
-            style={{height: "100%", width: "100%", display: "flex", alignItems: "center", justifyContent: "center"}}    
+            style={{height: "100%", width: "100%", display: "flex", alignItems: "center"}}    
         >
-            {/* bitcoin icon */}       
-            <div
-                style={{width: 1920, height: 1080, zIndex:1}}    
-            >
-                <Canvas
-                    camera={{ position: [0, 0, 100], fov: 90 }}                                                 
-                >
-                    <ModelRender />                
-                    <pointLight intensity={10} rotation={[200,0,0]} position={[0, 150, 100]} />                                
-                    {showAscii && <AsciiRenderer/>}
-                </Canvas>                
-            </div>
-
             {/* text */}            
             <div            
-                style={{zIndex:10, position: "absolute", left:0, right:0, top:0, bottom:0}}
+                style={{ flexGrow: 1 }}
             >
                 <div
-                    style={{height: "100%", width: "100%", display: "flex", alignItems: "center", justifyContent: "center"}}    
+                    style={{display: "flex", alignItems: "center", justifyContent: "center"}}    
                 >
                     <div
-                        style={{background: "rgba(0,0,0,0.5)", fontSize: 30, paddingLeft: 20, paddingRight: 20, width: 500, color: "white"}}
+                        style={{
+                            background: "rgba(0,0,0,0.5)", 
+                            fontSize: 30, 
+                            paddingLeft: 20, 
+                            paddingRight: 20, 
+                            width: 500, 
+                            color: "white",
+                            fontFamily: "'Silkscreen', cursive"
+                        }}
                     >
                         <Typewriter
                             key={textIndex.toString()}
-                            options={{delay: 1}}
+                            options={{delay: 45}}
                             onInit={(typewriter) => {                                
                                 typewriter
                                     .typeString(textArray[textIndex])
@@ -75,6 +70,24 @@ const BitcoinSlide = () => {
                     </div>
                 </div>                
             </div>
+            
+            {/* bitcoin icon */}       
+            <div
+            style={{ flexGrow: 1 }}
+            >
+                <div
+                    style={{width: 600, height: 600 }}    
+                >            
+                    <Canvas
+                        camera={{ position: [0, 0, 100], fov: 90 }}                                                 
+                    >
+                        <ModelRender />                
+                        <pointLight intensity={10} rotation={[200,0,0]} position={[0, 150, 100]} />                                
+                        {showAscii && <AsciiRenderer/>}
+                    </Canvas>                
+                </div>
+            </div>
+            
         </div>
     )
 }
